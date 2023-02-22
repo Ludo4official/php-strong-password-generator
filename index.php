fb2693b2e7bf38624ece9871af6590076fba055c
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-02-22 09:20:46 +0100

    PHP password generator

--- index.php
+++ index.php
@@ -15,13 +15,21 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     </head>
     <body>
-        <div class="container">
+        <div class="container bg-dark text-light">
             <div class="row">
                 <div class="col text-center my-5">
                      <h1>
                         PHP Strong Password Generator
                     </h1>
                     <hr>
+                </div>
+            <div class="row">
+                <div class="col text-center">
+                    <div class="mb-3">Seleziona la lunghezza della tua password:</div>
+                    <form action="">
+                        <input type="number" name="" id="">
+                        <button>Genera</button>
+                    </form>
                 </div>
             </div>
         </div>
